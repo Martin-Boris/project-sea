30815df8ecc7a1285f42106801ec90a010138dae
Boris <boris.martin02@laposte.net>
2025-02-02 17:05:58 +0100

    update wss url

--- core/src/main/java/com/bmrt/projectsea/websocket/WebsocketController.java
+++ core/src/main/java/com/bmrt/projectsea/websocket/WebsocketController.java
@@ -14,7 +14,7 @@ public class WebsocketController implements WebSocketPort {
 
     public WebsocketController(String shipName) {
         this.shipName = shipName;
-        socket = WebSockets.newSocket(WebSockets.toWebSocketUrl("159.69.212.196", 80));
+        socket = WebSockets.newSocket(WebSockets.toWebSocketUrl("project-bmrt.ip-ddns.com", 443));
     }
 
     @Override
